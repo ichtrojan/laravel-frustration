a68cfdc07c0d7d0a75113e48f29e599383fa6b89
ichtrojan <ordrizzy@gmail.com>
2020-05-15 16:17:04 +0100

    default frustration config

---
+++ publishable/config/frustration.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'routes' => [
+        'wp-admin.php', 'index.php', 'wp-login.php'
+    ],
+
+    'redirect' => [
+        'https://www.fbi.gov/wanted',
+        'https://www.youtube.com/watch?v=pok8H_KF1FA',
+        'https://fast.com',
+        'http://www.omfgdogs.com',
+        'https://cat-bounce.com'
+    ]
+];
